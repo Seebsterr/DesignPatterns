7c5535deb1e76ae5b14bc82df1a53616ce4c6850
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-04-30 12:04:03 +0200

    Property Proxy

---
+++ DesignPatterns/Proxy/PropertyProxy/Demo.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Proxy.PropertyProxy
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            var property = new Property<int>();
+            property.Value = 1;
+            property.Value = 1;
+        }
+    }
+}
---
+++ DesignPatterns/Proxy/PropertyProxy/Property.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Proxy.PropertyProxy
+{
+    public class Property<T> where T : new()
+    {
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (Equals(_value, value))
+                {
+                    return;
+                }
+
+                Console.WriteLine($"Assigning value to {value}");
+                _value = value;
+            }
+        }
+
+        private T _value;
+
+        public Property() : this(Activator.CreateInstance<T>()) { }
+
+        public Property(T value)
+        {
+            _value = value;
+        }
+
+        public static implicit operator T(Property<T> property)
+        {
+            return property.Value;
+        }
+
+        public static implicit operator Property<T>(T value)
+        {
+            return new Property<T>(value);
+        }
+    }
+}
