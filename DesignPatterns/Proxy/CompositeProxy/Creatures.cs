caee4b53e5fdc455b5e8ab8b67a5be9095c674e0
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-04-30 14:04:43 +0200

    Composite Proxy part 2

---
+++ DesignPatterns/Proxy/CompositeProxy/Creatures.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Proxy.CompositeProxy
+{
+    public class Creatures
+    {
+        public int Size { get; }
+        private byte[] _age;
+        private int[] _x;
+        private int[] _y;
+
+        public Creatures(int size)
+        {
+            Size = size;
+            _age = new byte[size];
+            _x = new int[size];
+            _y = new int[size];
+        }
+
+        public CreatureProxy GetElement(int index)
+        {
+            return new CreatureProxy(this, index);
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                yield return new CreatureProxy(this, i);
+            }
+        }
+
+        public struct CreatureProxy
+        {
+            private readonly Creatures _creatures;
+            private readonly int _index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                _creatures = creatures;
+                _index = index;
+            }
+
+            public ref byte Age => ref _creatures._age[_index];
+            public ref int X => ref _creatures._x[_index];
+            public ref int Y => ref _creatures._y[_index];
+        }
+    }
+}
--- DesignPatterns/Proxy/CompositeProxy/Demo.cs
+++ DesignPatterns/Proxy/CompositeProxy/Demo.cs
@@ -1,7 +1,26 @@
-﻿namespace DesignPatterns.Proxy.CompositeProxy
+﻿using System.Diagnostics;
+
+namespace DesignPatterns.Proxy.CompositeProxy
 {
     public class Demo
     {
+        public void Start()
+        {
+            // age x y age x y age x y
+            var creatures = new Creature[100];
+            foreach (var creature in creatures)
+            {
+                creature.X++;
+            }
 
+            // age age age
+            // x x x
+            // y y y
+            var creatures2 = new Creatures(100);
+            foreach (var creature in creatures2)
+            {
+                creature.X++;
+            }
+        }
     }
 }
