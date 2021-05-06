fcc937b93539e7e8946891a0b0bf515dcbb393e4
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-06 17:59:36 +0200

    Info about factory

---
+++ DesignPatterns/Factories/Info.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Factories
+{
+    public class Info
+    {
+        // Factory - a component responsible solely for the wholesale (not piecewise) creation of objects.
+
+        // Factory method 
+        // is a static method that creates objects
+        // Define an interface for creating an object, but let subclasses decide which class to instantiate. 
+        // Factory Method lets a class defer instantiation to subclasses.
+
+        // Abstract Factory
+        // Provide an interface for creating families of related or dependent objects without 
+        // specifying their concrete classes.
+    }
+}
