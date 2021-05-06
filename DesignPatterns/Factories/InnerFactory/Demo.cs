13ca24bff47606fddf55b229f9c1c8dc3e4731f6
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-06 09:17:13 +0200

    Inner Factory

---
+++ DesignPatterns/Factories/InnerFactory/Demo.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Factories.InnerFactory
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            var solarPoint = Point.Factory.NewPolarPoint(1.0, 2);
+            var cartesianPoint = Point.Factory.NewCatresianPoint(1.0, 2);
+            var origin = Point.Origin;
+
+            System.Console.WriteLine(solarPoint);
+            System.Console.WriteLine(cartesianPoint);
+        }
+    }
+}
---
+++ DesignPatterns/Factories/InnerFactory/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesignPatterns.Factories.InnerFactory
+{
+    public class Point
+    {
+        private double _x, _y;
+
+        private Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Point Origin = new Point(0, 0);
+
+        public static class Factory
+        {
+            public static Point NewCatresianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"x={_x}, y={_y}";
+        }
+    }
+}
