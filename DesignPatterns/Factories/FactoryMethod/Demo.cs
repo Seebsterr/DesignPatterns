4c575b1ad886629a5c2378bf1434c7e059130608
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-06 09:16:48 +0200

    FactoryMethod

---
+++ DesignPatterns/Factories/FactoryMethod/Demo.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns.Factories.FactoryMethod
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            var solarPoint = Point.NewPolarPoint(1.0, 2);
+            var cartesianPoint = Point.NewCatresianPoint(1.0, 2);
+
+            System.Console.WriteLine(solarPoint);
+            System.Console.WriteLine(cartesianPoint);
+        }
+    }
+}
---
+++ DesignPatterns/Factories/FactoryMethod/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesignPatterns.Factories.FactoryMethod
+{
+    public class Point
+    {
+        private double _x, _y;
+
+        public static Point NewCatresianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"x={_x}, y={_y}";
+        }
+    }
+}
