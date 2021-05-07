5483e9d0c00be6455ef3f32a80a1ccfc899a63c3
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-07 15:38:40 +0200

    Decorator Info

---
+++ DesignPatterns/Decorator/Info.cs
@@ -0,0 +1,6 @@
+﻿namespace DesignPatterns.Decorator
+{
+    // Decorator - Facilitates the addition of behaviors to individual objects without inheriting from them.
+    // Decorator keeps the reference to the decorated object(s)
+    // May or may not proxy over calls
+}
