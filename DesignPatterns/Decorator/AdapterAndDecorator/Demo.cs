93f3186e3b9895bb650fb612eb5b0768c4925dfe
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-07 14:11:06 +0200

    Adapter And Decorator

---
+++ DesignPatterns/Decorator/AdapterAndDecorator/Demo.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Decorator.AdapterAndDecorator
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            MyStringBuilder s = "hello ";
+            s += "world";
+
+            System.Console.WriteLine(s);
+        }
+    }
+}
