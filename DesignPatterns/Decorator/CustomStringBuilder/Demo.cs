83e69bd455354ad266194480263704fdfa34c0c6
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-07 14:10:54 +0200

    Custom String Builder (Decorator)

---
+++ DesignPatterns/Decorator/CustomStringBuilder/Demo.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Decorator.CustomStringBuilder
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            var codeBuilder = new CodeBuilder();
+            codeBuilder
+                .AppendLine("class Test")
+                .AppendLine("{")
+                .AppendLine("}");
+
+            System.Console.WriteLine(codeBuilder);
+        }
+    }
+}
