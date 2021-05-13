c5908a40275e8a852c17640261ae8fe1f62db9be
Sebastian Stenka <sebastian.stenka@dgt.eu>
2021-05-13 15:58:58 +0200

    FacadeBuilder

---
+++ DesignPatterns/Builder/FacadeBuilder/Demo.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Builder.FacadeBuilder
+{
+    public class Demo
+    {
+        public void Start()
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+              .Lives
+                .At("123 London Road")
+                .In("London")
+                .WithPostcode("SW12BC")
+              .Works
+                .At("Fabrikam")
+                .AsA("Engineer")
+                .Earning(123000);
+
+            System.Console.WriteLine(person);
+        }
+    }
+}
---
+++ DesignPatterns/Builder/FacadeBuilder/PersonBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Builder.FacadeBuilder
+{
+    public class PersonBuilder
+    {
+        protected Person Person { get; set; } = new Person();
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(Person);
+        public PersonJobBuilder Works => new PersonJobBuilder(Person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.Person;
+        }
+    }
+}
